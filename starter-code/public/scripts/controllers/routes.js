5652bccb0becd6786d9522dbe49f1123c3534cb3
Izabella Arya <izzy@izzys-magic-box.local>
2017-05-22 14:12:23 -0700

    added scripts for page.js

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,6 +1,13 @@
 'use strict';
+
 var app = app || {};
 
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
+page('/', articleController.index);
+page('/about', aboutController.index);
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
+//
+page();
+
+// DONE: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
+
+// DONE: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
